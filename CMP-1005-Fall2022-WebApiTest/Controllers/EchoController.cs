d5aaaaf04b441130a2f1078158c03ca82cdbddf9
Brent Ritchie <ritchie.brent@gmail.com>
2022-09-30 19:15:52 -0400

    Add project files.

---
+++ CMP-1005-Fall2022-WebApiTest/Controllers/EchoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMP_1005_Fall2022_WebApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string text)
+        {
+            return text;
+        }
+
+        [HttpPost]
+        public string Post(string text)
+        {
+            return text.ToUpper();
+        }
+
+        [HttpPut]
+        public string Put(string text)
+        {
+            return text.ToLower();
+        }
+    }
+}
---
+++ CMP-1005-Fall2022-WebApiTest/Controllers/MathController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMP_1005_Fall2022_WebApiTest.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpGet]
+        public int Add(int left, int right)
+        {
+            return left + right;
+        }
+
+        [HttpGet]
+        public int Subtract(int left, int right)
+        {
+            return left - right;
+        }
+    }
+}
